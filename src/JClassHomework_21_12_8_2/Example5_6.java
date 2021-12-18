2a63888df3f43858bbffbe75473f6e9d72b0a822
anyingiit <49945850+anyingiit@users.noreply.github.com>
2021-12-18 08:56:51 +0800

    Example5_6.java: 对关于抽象类和继承的补充, 补充了如何辨别实现多态位置的注释

--- src/JClassHomework_21_12_8_2/Example5_6.java
+++ src/JClassHomework_21_12_8_2/Example5_6.java
@@ -2,14 +2,16 @@ package JClassHomework_21_12_8_2;
 
 public class Example5_6 {
   public static void main(String[] args) {
-    // Animal animal = new Animal("someAnimal", "someColor");// 一旦定义了抽象类, 就不能单独实例化了
+    // Animal animal = new Animal("someAnimal", "someColor");// 一旦定义为抽象类, 就不能单独实例化了
+
+    // 单独实例化不属于多态
     Cat cat = new Cat("Cat", "white");
     Dog dog = new Dog("Dog", "black");
 
     System.out.println(cat.getName());
     System.out.println(dog.getName());
 
-    Raiser mike = new Raiser("Mike");
+    Raiser mike = new Raiser("Mike");// 在Raiser中实现了多态
     mike.feed(cat);// feed方法由Raiser定义, 会自动调用所有基于Animal基类的对象中的eat方法.
     mike.feed(dog);
 
